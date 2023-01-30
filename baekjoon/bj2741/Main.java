6b869daed0e58e58e4bb7433a246c9c20ba96fae
park-yeong-ki <okip0428@gmail.com>
2023-01-30 21:00:05 +0900

    solve: 백준 b5 N찍기

---
+++ baekjoon/bj2741/Main.java
@@ -0,0 +1,18 @@
+package bj2741;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Scanner 선언
+        Scanner sc = new Scanner(System.in);
+
+        //N 입력
+        int N = sc.nextInt();
+
+        //출력
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+}
