a717d2bb4ffee77f61fccb907947ed4c9423a9ee
park-yeong-ki <okip0428@gmail.com>
2023-02-25 11:25:17 +0900

    solve: 백준 s1 안전 영역

--- baekjoon/bj2468/Main.java
+++ baekjoon/bj2468/Main.java
@@ -43,7 +43,9 @@ public class Main {
                     //해당 좌표의 높이가 h 보다 크고 방문한 적이 없다면 안전한 영역이라 여기고 횟수를 센다.
                     if (region[i][j] > h && !visited[i][j]) {
                         //bfs -> 안전한 영역을 찾고 방문배열을 체크하기 위해 사용
-                        bfs(i, j, h);
+//                        bfs(i, j, h);
+                        //dfs -> 안전한 영역을 찾고 방문배열을 체크하기 위해 사용
+                        dfs(i, j ,h);
                         count++;
                     }
                 }
@@ -56,32 +58,45 @@ public class Main {
         System.out.println(result);
     }
 
-    //bfs구현
-    static void bfs(int x, int y, int h) {
-        //큐 생성
-        Queue<int[]> queue = new ArrayDeque<>();
-
-        //큐에 입력받은 좌표를 넣고 방문체크한다.
-        queue.offer(new int[]{x, y});
+    //dfs구현
+    static void dfs(int x, int y, int h) {
+        //방문체크
         visited[x][y] = true;
 
-        int[] current;
-        int r, c;
-        //큐가 비어있지 않을 때까지 반복
-        while (!queue.isEmpty()) {
-            //가장 처음 삽입한 큐를 제거
-            current = queue.poll();
-            r = current[0];
-            c = current[1];
-
-            //상하좌우의 방향으로 인접한 정점들을 탐색하며 방문하지 않은 안전한 영역을 탐색하도록 설정한다.
-            for (int i = 0; i < 4; i++) {
-                if (region[r + dx[i]][c + dy[i]] > h && !visited[r + dx[i]][c + dy[i]]) {
-                    //조건을 통과한 정점들을 큐에 넣고 방문체크
-                    queue.offer(new int[]{r + dx[i], c + dy[i]});
-                    visited[r + dx[i]][c + dy[i]] = true;
-                }
+        //상하좌우의 방향으로 인접한 정점들을 탐색하며 방문하지 않은 안전한 영역을 탐색하도록 설정한다.
+        for (int i = 0; i < 4; i++) {
+            if (region[x + dx[i]][y + dy[i]] > h && !visited[x + dx[i]][y + dy[i]]) {
+                dfs(x + dx[i], y + dy[i], h);
             }
         }
     }
+
+    //bfs구현
+//    static void bfs(int x, int y, int h) {
+//        //큐 생성
+//        Queue<int[]> queue = new ArrayDeque<>();
+//
+//        //큐에 입력받은 좌표를 넣고 방문체크한다.
+//        queue.offer(new int[]{x, y});
+//        visited[x][y] = true;
+//
+//        int[] current;
+//        int r, c;
+//        //큐가 비어있지 않을 때까지 반복
+//        while (!queue.isEmpty()) {
+//            //가장 처음 삽입한 큐를 제거
+//            current = queue.poll();
+//            r = current[0];
+//            c = current[1];
+//
+//            //상하좌우의 방향으로 인접한 정점들을 탐색하며 방문하지 않은 안전한 영역을 탐색하도록 설정한다.
+//            for (int i = 0; i < 4; i++) {
+//                if (region[r + dx[i]][c + dy[i]] > h && !visited[r + dx[i]][c + dy[i]]) {
+//                    //조건을 통과한 정점들을 큐에 넣고 방문체크
+//                    queue.offer(new int[]{r + dx[i], c + dy[i]});
+//                    visited[r + dx[i]][c + dy[i]] = true;
+//                }
+//            }
+//        }
+//    }
 }
