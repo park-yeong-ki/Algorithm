226a93a57b6203f9a20cd19680598e832c202522
park-yeong-ki <okip0428@gmail.com>
2023-03-15 23:27:36 +0900

    solve: 백준 s2 최대 힙

---
+++ baekjoon/bj11279/Main.java
@@ -0,0 +1,38 @@
+package bj11279;
+
+import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //연산의 개수 N
+        int N = Integer.parseInt(br.readLine());
+
+        //우선순위 큐 생성
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        //연산
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            //배열에서 가장 큰 값을 출력하고 그 값을 배열에서 제거하는 경우
+            if (num == 0) {
+                //배열이 비어 있는 경우인데 가장 큰 값을 출력하라고 한 경우에는 0을 출력
+                if (pq.isEmpty()) {
+                    bw.write(0 + "\n");
+                } else {
+                    bw.write(pq.poll() + "\n");
+                }
+            }
+            //배열에 x라는 값을 넣는(추가하는) 연산
+            else {
+                pq.offer(num);
+            }
+        }
+
+        bw.close();
+    }
+}
