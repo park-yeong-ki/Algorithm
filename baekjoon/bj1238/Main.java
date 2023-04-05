d39bc5e3409b0a0ad3f446e451683f131dd40ac1
park-yeong-ki <okip0428@gmail.com>
2023-04-05 23:55:44 +0900

    solve: 백준 g3 파티

---
+++ baekjoon/bj1238/Main.java
@@ -0,0 +1,101 @@
+package bj1238;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Edge implements Comparable<Edge>{
+        int num, weight;
+
+        public Edge(int num, int weight) {
+            this.num = num;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
+    static ArrayList<Edge>[] adjList;
+    static int N;
+    static int INF = Integer.MAX_VALUE;
+    static int[] count;
+    static int X;
+    static int[] dist;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        //간선리스트 생성
+        adjList = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            adjList[from].add(new Edge(to, weight));
+        }
+
+        count = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            if (i == X) continue;
+            dijkstra(i);
+        }
+        dijkstra(X);
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i <= N; i++) {
+            count[i] += dist[i];
+            max = Math.max(max, count[i]);
+        }
+
+        System.out.println(max);
+    }
+
+    static void dijkstra(int start){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+        boolean[] visited = new boolean[N + 1];
+        dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+
+        pq.offer(new Edge(start, 0));
+        dist[start] = 0;
+
+        Edge current;
+        while (!pq.isEmpty()) {
+            current = pq.poll();
+
+            if (start != X && current.num == X) {
+                count[start] += current.weight;
+                return;
+            }
+
+            if (visited[current.num]) continue;
+            visited[current.num] = true;
+
+            for (int i = 0; i < adjList[current.num].size(); i++) {
+                Edge edge = adjList[current.num].get(i);
+                if (!visited[edge.num] && dist[edge.num] > current.weight + edge.weight) {
+                    pq.offer(new Edge(edge.num, current.weight + edge.weight));
+                    dist[edge.num] = current.weight + edge.weight;
+                }
+            }
+        }
+    }
+}
