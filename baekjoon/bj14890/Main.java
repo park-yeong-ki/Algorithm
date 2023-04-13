1fcddee9e46663b3e0a83b751bae8329a2f8ed74
park-yeong-ki <okip0428@gmail.com>
2023-04-14 01:16:51 +0900

    solve: 백준 g3 경사로

--- baekjoon/bj14890/Main.java
+++ baekjoon/bj14890/Main.java
@@ -12,68 +12,37 @@ public class Main {
     static int street;
     public static void main(String[] args) throws IOException {
         input();
-        checkRow();
-        checkCol();
+        check("row");
+        check("col");
         System.out.println(street);
     }
-    static void checkRow() {
+    static void check(String dir) {
         for (int i = 0; i < N; i++) {
-            int h = arr[i][0];
+            int h = dir.equals("row") ? arr[i][0] : arr[0][i];
             int cnt = 1;
             boolean flag = true;
             for (int j = 1; j < N; j++) {
-                if (h == arr[i][j]) { //다음 칸과 높이가 같은 경우
+                int next = dir.equals("row") ? arr[i][j] : arr[j][i];
+                if (h == next) { //다음 칸과 높이가 같은 경우
                     cnt++;
                 } else {
                     int down = 0;
-                    if (arr[i][j] - h == 1) { //다음 칸이 1만큼 큰 경우
+                    if (next - h == 1) { //다음 칸이 1만큼 큰 경우
                         if (cnt < L) { //경사로를 놓을 칸이 되지 않는 경우
                             flag = false;
                             break;
                         }
-                    } else if (arr[i][j] - h == -1) { //다음 칸이 1만큼 작은 경우
+                    } else if (next - h == -1) { //다음 칸이 1만큼 작은 경우
                         if (cnt < 0) {
                             flag = false;
                             break;
                         }
                         down = L;
-                    } else if (Math.abs(arr[i][j] - h) > 1) { //칸의 높이차이가 1보다 큰 경우
+                    } else if (Math.abs(next - h) > 1) { //칸의 높이차이가 1보다 큰 경우
                         flag = false;
                         break;
                     }
-                    h = arr[i][j];
-                    cnt = 1 - down;
-                }
-            }
-            if (cnt >= 0 && flag) street++;
-        }
-    }
-    static void checkCol() {
-        for (int i = 0; i < N; i++) {
-            int h = arr[0][i];
-            int cnt = 1;
-            boolean flag = true;
-            for (int j = 1; j < N; j++) {
-                if (h == arr[j][i]) { //다음 칸과 높이가 같은 경우
-                    cnt++;
-                } else {
-                    int down = 0;
-                    if (arr[j][i] - h == 1) { //다음 칸이 1만큼 큰 경우
-                        if (cnt < L) { //경사로를 놓을 칸이 되지 않는 경우
-                            flag = false;
-                            break;
-                        }
-                    } else if (arr[j][i] - h == -1) { //다음 칸이 1만큼 작은 경우
-                        if (cnt < 0) {
-                            flag = false;
-                            break;
-                        }
-                        down = L;
-                    } else if (Math.abs(arr[j][i] - h) > 1) { //칸의 높이차이가 1보다 큰 경우
-                        flag = false;
-                        break;
-                    }
-                    h = arr[j][i];
+                    h = next;
                     cnt = 1 - down;
                 }
             }
