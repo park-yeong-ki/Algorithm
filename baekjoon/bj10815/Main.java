5c71a72288d66b65011fa6a43db8c5273aba0faf
park-yeong-ki <okip0428@gmail.com>
2023-06-10 23:48:45 +0900

    solve: 백준 s5 숫자 카드

---
+++ baekjoon/bj10815/Main.java
@@ -0,0 +1,32 @@
+package bj10815;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) sb.append(1 + " ");
+            else sb.append(0 + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ baekjoon/bj10815/Main2.java
@@ -0,0 +1,47 @@
+package bj10815;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Integer[] arr = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            int start = 0;
+            int end = N - 1;
+            while (start <= end) {
+                int mid = (start + end) / 2;
+
+                if (arr[mid] > target) {
+                    end = mid - 1;
+                } else if (arr[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    break;
+                }
+            }
+            if (start <= end) sb.append(1 + " ");
+            else sb.append(0 + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
