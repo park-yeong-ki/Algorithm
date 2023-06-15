0f3d0729a33e0c6553635f9aa80cc02e2ed17f50
park-yeong-ki <okip0428@gmail.com>
2023-06-15 15:24:17 +0900

    solve: 백준 g5 용액

---
+++ baekjoon/bj2467/Main.java
@@ -0,0 +1,44 @@
+package bj2467;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = N - 1;
+        int min = Integer.MAX_VALUE;
+        int ans1 = arr[start];
+        int ans2 = arr[end];
+        while (start < end) {
+            int num = arr[start] + arr[end];
+            if (min > Math.abs(num)) {
+                min = Math.abs(num);
+                ans1 = arr[start];
+                ans2 = arr[end];
+            }
+
+            if (num < 0) {
+                start++;
+            } else if (num > 0) {
+                end--;
+            } else {
+                start++;
+                end--;
+            }
+        }
+
+        System.out.println(ans1 + " " + ans2);
+    }
+}
