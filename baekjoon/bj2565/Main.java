1bc62a67e10461258aa17796073eb5515a74dab0
park-yeong-ki <okip0428@gmail.com>
2023-07-03 10:41:27 +0900

    solve: 백준 g5 전깃줄

---
+++ baekjoon/bj2565/Main.java
@@ -0,0 +1,43 @@
+package bj2565;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+
+
+        int[] dp = new int[N];
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i][1] > arr[j][1]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(N - ans);
+    }
+}
