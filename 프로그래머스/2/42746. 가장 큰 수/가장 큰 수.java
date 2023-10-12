d31a93925633e928d82dcaad52aab813244aa61e
park-yeong-ki <107581387+park-yeong-ki@users.noreply.github.com>
2023-10-12 23:59:35 +0900

    [level 2] Title: 가장 큰 수, Time: 2.44 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -8,26 +8,11 @@ class Solution {
             sArr[i] = String.valueOf(numbers[i]);
         }
         
-        //정렬하여 이어붙인다
+        //이어붙인값을 비교하며 정렬
         Arrays.sort(sArr, new Comparator<String>(){
             @Override
             public int compare(String s1, String s2){
-                int max = Math.max(s1.length(), s2.length());
-                
-                int num1;
-                int num2;
-                for(int i=0; i<2*max; i++){ //최대 두 문자열중 길이가 긴 문자열 기준으로 비교
-                    if(s1.length() <= i) num1 = s1.charAt(i % s1.length());
-                    else num1 = s1.charAt(i);
-                    if(s2.length() <= i) num2 = s2.charAt(i % s2.length());
-                    else num2 = s2.charAt(i);
-                    
-                    if(num1 != num2){ //비교 자리수의 값이 같지 않다면
-                        return num2 - num1; //내림차순 정렬
-                    }
-                }
-                
-                return 0; //이외에는 그대로
+                return Integer.parseInt(s2+s1) - Integer.parseInt(s1+s2);
             }
         });
         
@@ -36,9 +21,6 @@ class Solution {
             sb.append(sArr[i]);
         }
         
-        String ans = sb.toString();
-        if(ans.replace("0", "").length() == 0) return "0";
-        
-        return ans;
+        return sb.toString().charAt(0) == '0' ? "0" : sb.toString();
     }
 }
