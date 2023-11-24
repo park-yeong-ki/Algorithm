ca5438a0b7542e68e9f3d1b3d46ba11592caf011
park-yeong-ki <107581387+park-yeong-ki@users.noreply.github.com>
2023-11-25 00:52:48 +0900

    [Gold V] Title: AC, Time: 464 ms, Memory: 74192 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,14 +16,14 @@ public class Main {
             int n = Integer.parseInt(br.readLine());
 
             String arr = br.readLine();
-            LinkedList<Integer> list = new LinkedList<>(); //앞, 뒤만 제거하므로 링크드리스트 사용
+            ArrayDeque<String> arrayDeque = new ArrayDeque<>(); //앞, 뒤만 제거하므로 링크드리스트 사용
             StringBuilder sb = new StringBuilder();
             for(int j=0; j<arr.length(); j++){ //리스트에 숫자만 입력
                 char ch = arr.charAt(j);
 
                 if (ch < '0' || ch > '9') { //숫자가 아닌 경우
                     if (sb.length() == 0) continue; //아무것도 없는 경우
-                    list.add(Integer.parseInt(sb.toString()));
+                    arrayDeque.add(sb.toString());
                     sb.setLength(0);
                 } else {
                     sb.append(ch);
@@ -36,32 +37,32 @@ public class Main {
                 if (p.charAt(j) == 'R') { //뒤집기
                     flag = !flag;
                 } else { //첫번째 수 버리기
-                    if (list.isEmpty()){ //리스트에 제거할 숫자가 없는 경우
+                    if (arrayDeque.isEmpty()){ //리스트에 제거할 숫자가 없는 경우
                         ans.append("error\n");
                         isCompleted = false;
                         break;
                     }
 
                     if (flag) { //뒤집어진 경우
-                        list.removeLast(); //뒤에서 제거
+                        arrayDeque.removeLast(); //뒤에서 제거
                     } else { //정방향인 경우
-                        list.removeFirst(); //앞에서 제거
+                        arrayDeque.removeFirst(); //앞에서 제거
                     }
                 }
             }
 
             if (isCompleted) { //명령어를 모두 실행한 경우
-                int size = list.size();
+                int size = arrayDeque.size();
                 ans.append("[");
                 if (flag) { //최종적으로 뒤집혀진 경우
                     for (int j = 0; j < size; j++) {
                         if (j != 0) ans.append(",");
-                        ans.append(list.removeLast());
+                        ans.append(arrayDeque.removeLast());
                     }
                 } else {
                     for (int j = 0; j < size; j++) {
                         if (j != 0) ans.append(",");
-                        ans.append(list.removeFirst());
+                        ans.append(arrayDeque.removeFirst());
                     }
                 }
                 ans.append("]\n");
@@ -69,7 +70,7 @@ public class Main {
 
         }
 
-        System.out.println(ans);
+        System.out.print(ans);
 
     }
 }
