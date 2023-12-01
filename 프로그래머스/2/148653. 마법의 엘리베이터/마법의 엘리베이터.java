ea497c1f65ec64bcff3436e81f1c3044ad5818dd
park-yeong-ki <107581387+park-yeong-ki@users.noreply.github.com>
2023-12-01 19:02:46 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    static String str;
+    static int ans;
+    
+    public int solution(int storey) {
+        str = String.valueOf(storey);
+        ans = Integer.MAX_VALUE;
+        dfs(0, 0, false);
+        return ans;
+    }
+    
+    static void dfs(int depth, int cnt, boolean flag){
+        if(ans <= cnt) return; //가지치기
+        
+        if(str.length() == depth){
+            if(flag){ //다음 자리 수가 있는 경우
+                ans = Math.min(ans, cnt+1);
+            }else{ //다음 자리 수가 없는 경우
+                ans = Math.min(ans, cnt);
+            }
+            
+            return;
+        }
+        
+        int num = str.charAt(depth) - '0'; //현재 자리수
+        if(flag){ //만약 이전 버튼을 더해서 0으로 만든 경우
+            num = num+1;
+        }
+        
+        dfs(depth+1, cnt + (10 - num), true);//자리 버튼을 더해서 0으로 만드는 경우 -> 다음 자리수 +1
+        dfs(depth+1, cnt + num, false);//자리 버튼을 빼서 0으로 만드는 경우
+    }
+}
