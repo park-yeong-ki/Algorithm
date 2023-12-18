56815aecb38e699b9cc046de40bd271641088d47
park-yeong-ki <107581387+park-yeong-ki@users.noreply.github.com>
2023-12-18 21:54:45 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 5.69 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배 배달과 수거하기 - 150369 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369) 
 
 ### 성능 요약
 
-메모리: 90.9 MB, 시간: 5.38 ms
+메모리: 90.7 MB, 시간: 5.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 3:24:40
+2023년 12월 1일 21:54:42
 
 ### 문제 설명
 
--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,78 +1,67 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        //초기 택배 존재하는 가장 뒤 인덱스 저장
-        int dIdx = -1;
-        int pIdx = -1;
-        for(int i=deliveries.length-1; i>=0; i--){ //배달
-            if(deliveries[i] != 0){
-                dIdx = i;
-                break;
-            }
-        }
-        
-        for(int i=pickups.length-1; i>=0; i--){ //수거
-            if(pickups[i] != 0){
-                pIdx = i;
-                break;
-            }
-        }
+        int dIdx = n-1;
+        int pIdx = n-1;
         
         long ans = 0;
-        while(dIdx >= 0 || pIdx >= 0){ //배달, 수거 택배가 존재할때까지 반복
-            //총 이동거리 갱신
-            ans += 2*(Math.max(dIdx, pIdx) + 1);
+        while(true){ 
+            while(dIdx >= 0 && deliveries[dIdx] == 0){ //0이면 앞으로 땡김
+                dIdx--;
+            }
             
-            //택배 배달
+            while(pIdx >= 0 && pickups[pIdx] == 0){ //0이면 앞으로 땡김
+                pIdx--;
+            }
+                        
+            if(dIdx == -1 && pIdx == -1) break;
+            
+            ans += (Math.max(dIdx, pIdx) + 1) * 2;
             int dBox = cap;
-            for(int i=dIdx; i>=0; i--){
-                int cnt = deliveries[i]; //초기 박스 저장
-                
-                if(deliveries[i] > dBox){ //트럭 박스보다 많은 경우
+            for(int i=dIdx; i>=0; i--){ //배달 탐색
+                if(deliveries[i] > dBox){
                     deliveries[i] -= dBox;
-                }else { //트럭 박스보다 같거나 적은 경우
+                    dBox = 0;
+                }else if(deliveries[i] == dBox){
+                    deliveries[i] = 0;
+                    dBox = 0;
+                }else{
+                    dBox -= deliveries[i];
                     deliveries[i] = 0;
                 }
-
-                dBox -= cnt;
-                if(dBox <= 0 || i == 0){ //트럭 박스를 모두 배달하면 종료
-                    int idx = i;
-                    while(idx>=0){ //배달 택배가 존재하는 가장 마지막 인덱스 저장
-                        if(deliveries[idx] != 0){
-                            break;
-                        }
-                        idx--;
-                    }
-                    
-                    dIdx = idx;
+                
+                if(dBox == 0){ //가지고 간 배달박스를 모두 소거
+                    dIdx = i;
                     break;
                 }
             }
             
-            //택배 수거
+            if(dBox != 0){ //더이상 배달할 집이 없는 경우
+                dIdx = 0;
+            }
+            
             int pBox = cap;
-            for(int i=pIdx; i>=0; i--){
-                int cnt = pickups[i]; //초기 박스 저장
-                
-                if(pickups[i] > pBox){ //트럭 박스보다 많은 경우
+            for(int i=pIdx; i>=0; i--){ //수거 탐색
+                if(pickups[i] > pBox){
                     pickups[i] -= pBox;
-                }else { //트럭 박스보다 같거나 적은 경우
+                    pBox = 0;
+                }else if(pickups[i] == pBox){
+                    pickups[i] = 0;
+                    pBox = 0;
+                }else{
+                    pBox -= pickups[i];
                     pickups[i] = 0;
                 }
                 
-                pBox -= cnt;
-                if(pBox <= 0 || i == 0){ //트럭 박스를 모두 수거하면 종료
-                    int idx = i;
-                    while(idx>=0){ //수거 택배가 존재하는 가장 마지막 인덱스 저장
-                        if(pickups[idx] != 0){
-                            break;
-                        }
-                        idx--;
-                    }
-                    
-                    pIdx = idx;
+                if(pBox == 0){ //가지고 올 수 있는 수거박스를 모두 소거
+                    pIdx = i;
                     break;
                 }
             }
+            
+            if(pBox != 0){ //더이상 수거할 집이 없는 경우
+                pIdx = 0;
+            }
+            
         }
         
         return ans;
