e1877d998f251395c6292503f65c047dc4c99c78
park-yeong-ki <107581387+park-yeong-ki@users.noreply.github.com>
2024-01-23 23:39:23 +0900

    [level 2] Title: 양궁대회, Time: 0.30 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,52 +1,53 @@
-import java.util.*;
-
 class Solution {
-    static int[] ryan, apeach, ans;
+    static int[] apeach, ryan, ans;
     static int max;
-    static boolean flag;
+    
     public int[] solution(int n, int[] info) {
-        ryan = new int[11];
         apeach = info;
+        ryan = new int[11];
         max = 0;
         ans = new int[]{-1};
         
-        dfs(0, n);
+        dfs(0, n, 0);
         
         return ans;
     }
-            
-    static void dfs(int idx, int cnt){
+    
+    static void dfs(int idx, int arrow, int score){
         if(idx == 10){
-            ryan[idx] = cnt;
+            ryan[idx] = arrow;
             
-            int a = 0;
-            int r = 0;
-            boolean flag = false;
-            for(int i=0; i<=10; i++){
-                if(max > 0){
-                    if(ans[i] < ryan[i]) flag = true;
-                    else if(ans[i] > ryan[i]) flag = false;
+            if(max < score){
+                max = score;
+                ans = ryan.clone();
+            }else if(ans.length != 1 && max == score){
+                boolean flag = false;
+                for(int i=10; i>=0; i--){
+                    if(ans[i] < ryan[i]){
+                        flag = true;
+                        break;
+                    }else if(ans[i] > ryan[i]){
+                        break;
+                    }
                 }
                 
-                if(apeach[i] == 0 && ryan[i] == 0) continue;
-                if(apeach[i] < ryan[i]) r += 10 - i;
-                else a += 10 - i;
-            }
-            
-            if(max < r - a){
-                max = r - a;
-                ans = ryan.clone();
-            }else if(max == r - a && flag){
-                ans = ryan.clone();
+                if(flag) ans = ryan.clone();                
             }
             
             return;
         }
         
-        for(int i=0; i<=cnt; i++){
-            ryan[idx] = i;
-            dfs(idx+1, cnt-i);
+        if(apeach[idx] < arrow){
+            ryan[idx] = apeach[idx] + 1;
+            dfs(idx+1, arrow - (apeach[idx] + 1), score + (10 - idx));
+        }
+        
+        if(apeach[idx] == 0){
             ryan[idx] = 0;
+            dfs(idx+1, arrow, score);
+        } else{
+            ryan[idx] = 0;
+            dfs(idx+1, arrow, score - (10 - idx));
         }
     }
 }
