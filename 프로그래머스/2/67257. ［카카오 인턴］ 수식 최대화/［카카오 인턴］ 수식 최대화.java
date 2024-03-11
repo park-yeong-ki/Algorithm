231f106c96b62248d7cafb409247d70c375cb933
park-yeong-ki <107581387+park-yeong-ki@users.noreply.github.com>
2024-03-11 23:02:00 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 8.50 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,121 +1,97 @@
 import java.util.*;
 
 class Solution {
-    static char[] cArr;
-    static char[] pArr;
+    static long ans;
+    static char[] oArr;
     static boolean[] visited;
-    static long max;
-    static String origin;
-    
     public long solution(String expression) {
-        expression = expression.replace("+", "a");
-        expression = expression.replace("-", "b");
-        expression = expression.replace("*", "c");
-        
-        origin = expression;
-        
-        String s = expression.replaceAll("[0-9]", "");
-        
-        Set<Character> set = new HashSet();
-        for(int i=0; i<s.length(); i++){
-            set.add(s.charAt(i));
-        }
-        
-        Character[] arr = set.toArray(new Character[0]);
-        
-        cArr = new char[arr.length];
-        for(int i=0; i<arr.length; i++){
-            cArr[i] = arr[i];
-        }
+        Set<Character> set = new HashSet<>();
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<expression.length(); i++){
+            if(expression.charAt(i) < '0' || expression.charAt(i) > '9'){
+                set.add(expression.charAt(i));
                 
-        visited = new boolean[cArr.length];
-        pArr = new char[cArr.length];
-        max = Long.MIN_VALUE;
-        
-        perm(0); // 연산자 순열
-        
-        return max;
-    }
+                list.add(sb.toString());
+                sb.setLength(0);
     
-    static String calculate(char ch, String str){  
-        
-        //해당 연산자가 존재하는지 확인 후 연산한 문자열 반환
-        for(int i=0; i<str.length(); i++){
-            if(str.charAt(i) == ch){                
-                //좌측 문자열, 우측 문자열 구하기
-                int left = i-1;
-                int right = i+1;
-                
-                StringBuilder sb = new StringBuilder();
-                while(0 <= left){
-                    if(str.charAt(left) == 'a' || str.charAt(left) == 'b' || str.charAt(left) == 'c') break; //다음 연산자를 만나면 종료
-                    sb.append(str.charAt(left));
-                    left--;
-                }
-                left++;
-                
-                long leftNum = Long.parseLong(sb.reverse().toString());
-                sb.setLength(0);
-                        
-                while(right < str.length()){
-                    if(str.charAt(right) == 'a' || str.charAt(right) == 'b' || str.charAt(right) == 'c') break; //다음 연산자를 만나면 종료
-                    sb.append(str.charAt(right));
-                    right++;
-                }
-                
-                long rightNum = Long.parseLong(sb.toString());
-                sb.setLength(0);
-                                
-                String prevS = str.substring(0, left);
-                String nextS = str.substring(right, str.length());
-                
-                long result = 0;
-                switch (ch){
-                    case 'a':
-                        result = leftNum + rightNum;
-                        break;
-                    case 'b':
-                        result = leftNum - rightNum;
-                        break;
-                    case 'c':
-                        result = leftNum * rightNum;
-                        break;
-                }
-                
-                sb.append(prevS).append(result).append(nextS);
-                
-                return sb.toString();
+                list.add(expression.charAt(i) + "");
+            }else{
+                sb.append(expression.charAt(i));
             }
         }
+        list.add(sb.toString());
+        sb.setLength(0);
         
-        return str;
+        oArr = new char[set.size()];
+        int size = 0;
+        for(char o : set){
+            oArr[size++] = o;
+        }
+        
+        visited = new boolean[oArr.length];
+        ans = 0;
+        perm(0, list);
+        
+        return ans;
     }
     
-    static void perm(int idx){ 
-        if(idx == cArr.length){
-            String str = origin;
-            
-            //문자열 돌며 새로운 연산자 우선순위를 적용하여 최대값 갱신
-            for(int i=0; i<pArr.length; i++){
-                while(true){
-                    String prev = str; 
-                    str = calculate(pArr[i], str);
-                    
-                    if(prev.equals(str)) break; //더이상 변화가 없으면 루프문 종료
-                }
-            }
-            
-            max = Math.max(max, Math.abs(Long.parseLong(str))); //최대값 갱신
-            
+    static void perm(int idx, List<String> sList){
+        if(idx == oArr.length){
+            ans = Math.max(ans, Math.abs(Long.parseLong(sList.get(0))));
             return;
         }
         
-        for(int i=0; i<cArr.length; i++){
+        for(int i=0; i<oArr.length; i++){
             if(visited[i]) continue;
             visited[i] = true;
-            pArr[idx] = cArr[i];
-            perm(idx+1);
+            perm(idx+1, calculate(sList, oArr[i]));
             visited[i] = false;
         }
+    }
+    
+    static List<String> calculate(List<String> sList, char o){
+        List<String> nList = new ArrayList<>();
+        
+        switch(o){
+            case '+':
+                for(int i=0; i<sList.size(); i++){
+                    if(sList.get(i).equals("+")){
+                        long prev = Long.parseLong(nList.get(nList.size()-1));
+                        long next = Long.parseLong(sList.get(i+1));
+                        nList.set(nList.size()-1, String.valueOf(prev+next));
+                        i++;
+                    }else{
+                        nList.add(sList.get(i));
+                    }
+                }
+                break;
+            case '-':
+                for(int i=0; i<sList.size(); i++){
+                    if(sList.get(i).equals("-")){
+                        long prev = Long.parseLong(nList.get(nList.size()-1));
+                        long next = Long.parseLong(sList.get(i+1));
+                        nList.set(nList.size()-1, String.valueOf(prev-next));
+                        i++;
+                    }else{
+                        nList.add(sList.get(i));
+                    }
+                }
+                break;
+            case '*':
+                for(int i=0; i<sList.size(); i++){
+                    if(sList.get(i).equals("*")){
+                        long prev = Long.parseLong(nList.get(nList.size()-1));
+                        long next = Long.parseLong(sList.get(i+1));
+                        nList.set(nList.size()-1, String.valueOf(prev*next));
+                        i++;
+                    }else{
+                        nList.add(sList.get(i));
+                    }
+                }
+                break;
+        }
+        
+        return nList;
     }
 }
