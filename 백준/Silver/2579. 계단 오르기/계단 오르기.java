ed6385261d05afe268c8b6577ed4ca3bf53f68fc
park-yeong-ki <107581387+park-yeong-ki@users.noreply.github.com>
2024-04-04 20:51:20 +0900

    [Silver III] Title: 계단 오르기, Time: 128 ms, Memory: 13424 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[][] dp = new int[n + 1][3];
+        dp[1][1] = arr[1];
+        dp[1][2] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][1] = Math.max(dp[i - 2][1], dp[i - 2][2]) + arr[i];
+            dp[i][2] = dp[i - 1][1] + arr[i];
+        }
+
+        System.out.println(Math.max(dp[n][1], dp[n][2]));
+    }
+}
