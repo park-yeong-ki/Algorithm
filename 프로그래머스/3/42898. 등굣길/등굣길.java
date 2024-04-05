0c6d7f305415925598e72c46da42a338551416a7
park-yeong-ki <107581387+park-yeong-ki@users.noreply.github.com>
2024-04-05 23:33:28 +0900

    [level 3] Title: 등굣길, Time: 0.75 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,37 +1,21 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int col = m;
-        int row = n;
+        int[][] dp = new int[n+1][m+1];
         
-        int[][] dp = new int[row+1][col+1];
-        dp[1][1] = 1;
-        
-        boolean[][] water = new boolean[row+1][col+1];
         for(int i=0; i<puddles.length; i++){
-            int r = puddles[i][1];
-            int c = puddles[i][0];
-            water[r][c] = true;
+            dp[puddles[i][1]][puddles[i][0]] = -1;
         }
         
-        int mod = 1000000007;
-        for(int i=1; i<=row; i++){
-            for(int j=1; j<=col; j++){
-                if(dp[i][j] == 0){
-                    continue;
-                }else{
-                    if(i+1 <= row && !water[i+1][j]){
-                        dp[i+1][j] += dp[i][j];
-                        dp[i+1][j] %= mod;
-                    }
-                    
-                    if(j+1 <= col && !water[i][j+1]){
-                        dp[i][j+1] += dp[i][j];
-                        dp[i][j+1] %= mod;
-                    }
-                }
+        dp[1][1] = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(dp[i][j] == -1) continue;
+                if(dp[i-1][j] != -1) dp[i][j] += dp[i-1][j];
+                if(dp[i][j-1] != -1) dp[i][j] += dp[i][j-1];
+                dp[i][j] %= 1000000007;
             }
         }
         
-        return dp[row][col];
+        return dp[n][m];
     }
 }
