29bd12f4d5c643b6a8544512165976826c97c8ac
park-yeong-ki <107581387+park-yeong-ki@users.noreply.github.com>
2024-04-06 01:01:55 +0900

    [level 3] Title: 산 모양 타일링, Time: 6.61 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/README.md
+++ 프로그래머스/3/258705. 산 모양 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 3] 산 모양 타일링 - 258705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258705?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258705) 
 
 ### 성능 요약
 
-메모리: 83.2 MB, 시간: 5.93 ms
+메모리: 77.9 MB, 시간: 6.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 02일 19:57:07
+2024년 04월 06일 01:01:52
 
 ### 문제 설명
 
--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,21 +1,15 @@
 class Solution {
     public int solution(int n, int[] tops) {
-        int[] a = new int[n];
-        int[] b = new int[n];
+        int[][] dp = new int[n][2];
+        dp[0][0] = 1;
+        dp[0][1] = tops[0] == 1 ? 3 : 2;
         
-        a[0] = 1; //역삼각형을 오른쪽 정삼각형과 함께 덮는 방법
-        b[0] = tops[0] == 1 ? 3 : 2; //역삼각형을 덮는 그외 방법
         int mod = 10007;
         for(int i=1; i<tops.length; i++){
-            if(tops[i] == 1){
-                a[i] = (a[i-1] + b[i-1]) % mod;
-                b[i] = (a[i-1] * 2 + b[i-1] * 3) % mod;
-            }else{
-                a[i] = (a[i-1] + b[i-1]) % mod;
-                b[i] = (a[i-1] + b[i-1] * 2) % mod;
-            }
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1]) % mod;
+            dp[i][1] = (dp[i-1][0] * (1+tops[i]) + dp[i-1][1] * (2+tops[i])) % mod;
         }
         
-        return (a[n-1] + b[n-1]) % mod;
+        return (dp[n-1][0] + dp[n-1][1]) % mod;
     }
 }
